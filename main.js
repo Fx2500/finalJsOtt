b6fe51b13c9b7e93c2c3e23e41be5cb2c328effa
Fx2500 <d.holz@live.com>
2021-09-20 15:26:34 -0300

    adding initial files

---
+++ main.js
@@ -0,0 +1,4 @@
+//Aqui terá o algoritimo que importa o obj do json e imprime as compras pro cliente, organizadas por data e o total de gastos.
+
+// comando de chamada da função 'node --experimental-json-modules main.js'
+
---
+++ math.js
@@ -0,0 +1,13 @@
+//algoritimos calculos
+
+const clients = [
+    {"id": 1, "name": "Érico"},
+    {"id": 2, "name": "Juca"}
+  ];
+const purchases = [
+    {"clients_id": 1, "data": "11/04/2021", "total": 100.00},
+    {"clients_id": 2, "data": "11/03/2021", "total": 83.71},
+    {"clients_id": 2, "data": "08/02/2021", "total": 52.48},
+    {"clients_id": 1, "data": "20/04/2021", "total": 329.99}
+  ];
+
